da7e311d2a7e44a400cee29f815184b772a495b4
IvanKristiawan <101783828+IvanKristiawan@users.noreply.github.com>
2023-04-07 09:05:56 +0700

    Fix Profil User

--- src/pages/Utility/ProfilUser/ProfilUser.jsx
+++ src/pages/Utility/ProfilUser/ProfilUser.jsx
@@ -1,18 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { AuthContext } from "../../../contexts/AuthContext";
-import { useStateContext } from "../../../contexts/ContextProvider";
+import { tempUrl, useStateContext } from "../../../contexts/ContextProvider";
 import { Container, Form, Row, Col } from "react-bootstrap";
 import { Button, ButtonGroup } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 
 const ProfilUser = () => {
   const { screenSize } = useStateContext();
+  const [masaBerlaku, setMasaBerlaku] = useState("");
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const textRight = {
     textAlign: screenSize >= 650 && "right"
+  };
+
+  useEffect(() => {
+    getAktivasiById();
+  }, []);
+
+  const getAktivasiById = async () => {
+    const response = await axios.post(`${tempUrl}/aktivasisByUser`, {
+      userId: user.id,
+      _id: user.id,
+      token: user.token
+    });
+    let newMasaAktif = new Date(response.data.masaAktif);
+    let tempMasaAktif = `${newMasaAktif.getDate().toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    })}-${(newMasaAktif.getMonth() + 1).toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    })}-${newMasaAktif.getFullYear()}`;
+    setMasaBerlaku(tempMasaAktif);
   };
 
   return (
@@ -68,6 +91,42 @@ const ProfilUser = () => {
               </Form.Group>
             </Col>
           </Row>
+          {user.tipeUser === "MEMBER" && (
+            <>
+              <Row>
+                <Col sm={6}>
+                  <Form.Group
+                    as={Row}
+                    className="mb-3"
+                    controlId="formPlaintextPassword"
+                  >
+                    <Form.Label column sm="3" style={textRight}>
+                      Deposit :
+                    </Form.Label>
+                    <Col sm="9">
+                      <Form.Control value={user.deposit} disabled readOnly />
+                    </Col>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col sm={6}>
+                  <Form.Group
+                    as={Row}
+                    className="mb-3"
+                    controlId="formPlaintextPassword"
+                  >
+                    <Form.Label column sm="3" style={textRight}>
+                      Masa Berlaku :
+                    </Form.Label>
+                    <Col sm="9">
+                      <Form.Control value={masaBerlaku} disabled readOnly />
+                    </Col>
+                  </Form.Group>
+                </Col>
+              </Row>
+            </>
+          )}
         </Form>
       </Container>
     </Container>
